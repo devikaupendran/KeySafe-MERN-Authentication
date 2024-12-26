8a8ce5f5bcd2985f7d8cf84bf15626c1b18d8486
devikaupendran <devikaupendranr@gmail.com>
2024-12-26 12:27:37 +0530

    create sign in api route

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js"
 import { errorHandler } from '../utils/error.js'
 import bcryptjs from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 
 export const signup = async (req, res, next) => {
 
@@ -16,3 +17,24 @@ export const signup = async (req, res, next) => {
     }
 }
 
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) return next(errorHandler(404, 'User not found')); //checking user is valid or not
+
+        const validPassword = bcryptjs.compareSync(password, validUser.password)
+        if (!validPassword) return next(errorHandler(401, 'Invalid credentials')) //checking password is valid or not
+
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+        const { password: hashedPassword, ...rest } = validUser._doc; //stores everything except password
+
+        //setting a expiry date for cookie
+        const expiryDate = new Date(Date.now() + 3600000) // 1 hr
+        res.cookie('access_token', token, { httpOnly: true, expires: expiryDate }).status(200).json(rest)
+    }
+    catch (error) {
+        next(error)
+    }
+
+}
