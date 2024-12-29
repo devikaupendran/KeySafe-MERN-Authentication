1179ce643eed920a72a0bd26d9f076a543ad6778
devikaupendran <devikaupendranr@gmail.com>
2024-12-29 13:10:01 +0530

    Complete signIn page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 export default function SignUp() {
     const [formData, setFormData] = useState({});
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
+
+    const navigate = useNavigate()
 
     const handleChange = (e) => {
         setFormData(
@@ -34,8 +36,8 @@ export default function SignUp() {
             if(data.success === false) {
                 setError(true)
                 return;
-            }
-            
+            }  
+            navigate('/sign-in')
         }
         catch (error) {
             setLoading(false);
