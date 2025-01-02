a3e9054619061035d051cbd8e69f7a97963c4933
devikaupendran <devikaupendranr@gmail.com>
2025-01-03 00:22:42 +0530

    Added some changes

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,19 @@ import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js'
 import authRoutes from './routes/auth.route.js'
 import cookieParser from 'cookie-parser'
+import path from 'path'
 
+const __dirname = path.resolve()
 //initializing
 dotenv.config();
 const app = express();
 app.use(express.json())
 app.use(cookieParser())
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+})
 
 mongoose.connect(process.env.MONGO).then(() => {
     console.log('connected to mongoDB')
@@ -30,7 +37,7 @@ app.use('/api/auth', authRoutes)
 
 //middleware for handling error
 app.use((err, req, res, next) => {
-    
+
     const statusCode = err.statusCode || 500;
     const message = err.message || 'internal server error';
 
